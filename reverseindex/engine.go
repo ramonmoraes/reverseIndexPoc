f3277765f4c35a836079e0b0db5d1b1530586013
Ramon Moraes <ramon.moraes94@gmail.com>
2019-07-02 17:44:03 -0300

    create basic engine

---
+++ reverseindex/engine.go
@@ -0,0 +1,34 @@
+package reverseindex
+
+const engineFileSystemPath = "./\\.reverseindex"
+
+type ReverseIndex map[token][]documentPath
+type Corpus map[documentPath]document
+
+type Engine struct {
+	ReverseIndex ReverseIndex
+	Corpus Corpus
+}
+
+func (eng Engine) index(input string) {
+	tokens := tokenizer(input)
+	document := createDocument(input)
+
+	for _, token := range tokens {
+		eng.ReverseIndex[token] = append(eng.ReverseIndex[token], document.path)
+	}
+}
+
+func (eng Engine) search(input string) []documentPath {
+	tokens := tokenizer(input)
+
+	paths := []documentPath{}
+	for _, token := range(tokens) {
+		paths = append(paths, eng.ReverseIndex[token]...)
+	}
+	return mergePaths(paths)
+}
+
+func mergePaths(paths []documentPath) []documentPath {
+	return paths
+}
